c8fc84a0668ac74e62a344972347839a0cb1c0c6
LuisaFGO <fgiraldo070@gmail.com>
2024-05-29 19:49:57 -0500

    updated environment

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -1,25 +1,34 @@
 import nodemailer from 'nodemailer';
-import { NODEMAILER_EMAIL, NODEMAILER_PASSWORD } from '../utils/environment';
+import {
+  FOR_EMAIL,
+  NODEMAILER_EMAIL,
+  NODEMAILER_PASSWORD,
+} from '../utils/environment';
 import htmlTemplate from './template';
 
-const createTransporter = () =>{
-  const transporter =  nodemailer.createTransport({
+const createTransporter = () => {
+  const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: NODEMAILER_EMAIL,
-      pass: NODEMAILER_PASSWORD
-    }
+      pass: NODEMAILER_PASSWORD,
+    },
   });
   return transporter;
 };
 
-export const sendEmail = async (name: string, phone: string, email: string, message: string) => {
+export const sendEmail = async (
+  name: string,
+  phone: string,
+  email: string,
+  message: string,
+) => {
   const transporter = createTransporter();
   const info = {
     from: `<${email}>`,
-    to: NODEMAILER_EMAIL,
+    to: FOR_EMAIL,
     subject: 'New message from the contact form',
-    html: htmlTemplate(name, phone, email, message)
+    html: htmlTemplate(name, phone, email, message),
   };
   await transporter.sendMail(info);
 };
--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -4,3 +4,4 @@ dotenv.config();
 export const PORT = process.env.PORT;
 export const NODEMAILER_EMAIL = process.env.NODEMAILER_EMAIL;
 export const NODEMAILER_PASSWORD = process.env.NODEMAILER_PASSWORD;
+export const FOR_EMAIL = process.env.FOR_EMAIL;
